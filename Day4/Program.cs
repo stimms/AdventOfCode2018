520b92f39b1603a75f5ceaa786461571ce2cd6ac
Simon Timms <stimms@gmail.com>
2018-12-03 20:49:14 -0700

    Starting setup for day 4

---
+++ Day4/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.IO;
+
+namespace Day4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Starting");
+
+            var lines = File.ReadAllLines("input.txt");
+            foreach(var line in lines){
+                
+            }
+            Console.WriteLine("done.");
+            Console.ReadLine();
+        }
+    }
+}
