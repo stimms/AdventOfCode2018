2cfb3044d31ea28df3ca6a814ac201c65ec30bf1
Simon Timms <stimms@gmail.com>
2018-12-04 21:50:10 -0700

    Start for day 5

---
+++ Day5/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.IO;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Day5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Starting");
+
+            var lines = File.ReadAllLines("input.txt");
+            foreach (var line in lines)
+            {
+                var (a, b) = line.Split(new char[] { ',' });
+            }
+            Console.WriteLine("done.");
+            Console.ReadLine();
+        }
+    }
+}
