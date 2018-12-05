3c5b7b898aa729dfca0cf9c317cd326f361af7ca
Simon Timms <stimms@gmail.com>
2018-12-04 22:38:34 -0700

    Implementation for day 5

--- Day5/Program.cs
+++ Day5/Program.cs
@@ -7,15 +7,45 @@ namespace Day5
 {
     class Program
     {
+        private const string alphabet = "abcdefghijklmnopqrstuvwxyz";
+
+        private static string React(string line)
+        {
+            var length = line.Length;
+            do
+            {
+                length = line.Length;
+                foreach (var c in alphabet)
+                {
+                    line = line.Replace($"{c}{c.ToString().ToUpper()}", "").Replace($"{c.ToString().ToUpper()}{c}", "");
+                }
+               
+            }
+            while (length != line.Length);
+            return line;
+        }
         static void Main(string[] args)
         {
             Console.WriteLine("Starting");
 
             var lines = File.ReadAllLines("input.txt");
-            foreach (var line in lines)
+            Console.WriteLine(React(lines.First()).Length);
+
+            var lengths = new List<(string, int)>();
+            foreach (var c in alphabet)
             {
-                var (a, b) = line.Split(new char[] { ',' });
+                var currentReplacement = c.ToString();
+                var line = lines.First();
+                
+                line = line.Replace(currentReplacement, "");
+                line = line.Replace(currentReplacement.ToUpper(), "");
+                line = React(line);
+                lengths.Add((c.ToString(), line.Length));
+                Console.Write(".");
+
             }
+            Console.WriteLine(lengths.Min(x => x.Item2));
+
             Console.WriteLine("done.");
             Console.ReadLine();
         }
