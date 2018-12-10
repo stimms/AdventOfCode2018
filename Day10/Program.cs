4c799823217b0aae178b91853d79942ac621050e
Simon Timms <stimms@gmail.com>
2018-12-09 21:50:22 -0700

    Outline for day 10

---
+++ Day10/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+
+namespace Day10
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var line = File.ReadAllLines("input.txt").First();
+           //var numbers = line.Split(' ').Select(x => Int32.Parse(x));
+            Console.WriteLine("Done.");
+            Console.ReadLine();
+        }
+    }
+}
