1aa3e6e6934a94dbc418750cda9fac3323f3b223
Simon Timms <stimms@gmail.com>
2018-12-12 00:15:41 -0700

    DA12

--- Day12/Program.cs
+++ Day12/Program.cs
@@ -14,47 +14,78 @@ namespace Day12
         static void Main(string[] args)
         {
             var lines = File.ReadAllLines("input.txt");
-            var state = new List<bool>();
-            for (int i = 0; i < 20; i++)
-                state.Add(false);
+            var state = new HashSet<long>();
+
+            int charCounter = 0;
             foreach (var c in lines.First())
             {
-                if (c == '.')
-                    state.Add(false);
-                else
-                    state.Add(true);
+                if (c == '#')
+                    state.Add(charCounter);
+                charCounter++;
             }
-            for (int i = 0; i < 20; i++)
-                state.Add(false);
+
             var rules = new List<(bool, bool, bool, bool, bool, bool)>();
             foreach (var line in lines.Skip(2))
             {
                 rules.Add((line[0] == '#', line[1] == '#', line[2] == '#', line[3] == '#', line[4] == '#', line[9] == '#'));
             }
-            Console.WriteLine(String.Join(' ', state.Skip(17).Take(35).Select(x => x ? "#" : ".")).Replace(" ", ""));
-            for (int i = 0; i < 20; i++)
+            var seenStates = new List<(long, List<long>, HashSet<long>)>();
+            var normalizationFactor = state.Min();
+            var normalizedState = state.Select(x => x - normalizationFactor).ToList();
+            seenStates.Add((normalizationFactor, normalizedState, state));
+            var jumped = false;
+            for (long i = 0; i < 50_000_000_000; i++)
             {
-                var newState = new List<bool> ();
-                for (int j = 0; j < state.Count; j++)
-                    newState.Add(false);
-                for (int j = 2; j < state.Count - 2; j++)
+                var newState = new HashSet<long>();
+
+                for (long j = state.Min() - 2; j < state.Max() + 2; j++)
                 {
 
-                    (bool, bool, bool, bool, bool, bool) matchingRule = rules.SingleOrDefault(x => x.Item1 == state[j - 2] &&
-                            x.Item2 == state[j - 1] &&
-                            x.Item3 == state[j] &&
-                            x.Item4 == state[j + 1] &&
-                            x.Item5 == state[j + 2]);
-                    newState[j] = rules.Any(x => x.Item1 == state[j - 2] &&
-                            x.Item2 == state[j - 1] &&
-                            x.Item3 == state[j] &&
-                            x.Item4 == state[j + 1] &&
-                            x.Item5 == state[j + 2]) ? matchingRule.Item6 : false;
+                    (bool, bool, bool, bool, bool, bool) matchingRule = rules.SingleOrDefault(x => x.Item1 == state.Contains(j - 2) &&
+                            x.Item2 == state.Contains(j - 1) &&
+                            x.Item3 == state.Contains(j) &&
+                            x.Item4 == state.Contains(j + 1) &&
+                            x.Item5 == state.Contains(j + 2));
+                    if (matchingRule.Item6)
+                        newState.Add(j);
                 }
-                Console.WriteLine(String.Join(' ', state.Skip(17).Take(35).Select(x => x ? "#" : ".")).Replace(" ", ""));
+
                 state = newState;
+                normalizationFactor = state.Min();
+                normalizedState = state.Select(x => x - normalizationFactor).ToList();
+
+                var seen = false;
+                if (!jumped)
+                {
+                    foreach (var seenState in seenStates)
+                    {
+                        if (seenState.Item2.Zip(normalizedState, (a, b) => a - b).All(x => x == 0))
+                        {
+                            Console.WriteLine("Loop detected at " + i + " and normalization factor " + normalizationFactor);
+                            seen = true;
+                            long blockSize = i - seenState.Item1;
+                            var blockCount = ((50_000_000_000 - i) / blockSize);
+                            var destItteration = i + (blockCount * (blockSize));
+
+                            state = newState.Select(x => (x + (blockCount * blockSize))).ToHashSet();
+                            i = destItteration;
+                            Console.WriteLine();
+                            jumped = true;
+                        }
+                    }
+                    if (!seen)
+                    {
+                        seenStates.Add((normalizationFactor, normalizedState, newState));
+                    }
+                }
+
             }
-            Console.WriteLine(state.Select((b, i) => b ? i - 20 : 0).Sum());
+            long sum = 0;
+            for (int i = 0; i < state.Count; i++)
+            {
+                sum += state.ToList()[i];
+            }
+            Console.WriteLine(sum);
             Console.WriteLine("done.");
             Console.ReadLine();
         }
