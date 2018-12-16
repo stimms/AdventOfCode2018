297cd97e7e887ce4323fcd1a4d4cf810dc216034
Simon Timms <stimms@gmail.com>
2018-12-15 23:26:03 -0700

    Part 2

--- Day16/Program.cs
+++ Day16/Program.cs
@@ -37,10 +37,14 @@ namespace Day16
                 new Eqri(),
                 new Eqrr()
             };
-            Assert.Equal(16, ops.Distinct().Count());
-            //var input = (new int[] { 3, 2, 1, 1 }, new int[] { 9, 2, 1, 2 }, new int[] { 3, 2, 2, 1 });
+            Assert.Equal(16, ops.Select(x => x.GetType().Name).Distinct().Count());
             int counter = 0;
             var inputs = GetInputs();
+
+            var possibleOpMapping = new List<List<OpCode>>();
+            for (int i = 0; i < ops.Count; i++)
+                possibleOpMapping.Add(ops.Select(x => x).ToList());
+
             foreach (var input in inputs)
             {
                 var same = 0;
@@ -51,15 +55,49 @@ namespace Day16
                     output[input.Item2[3]] = code.Execute(input.Item2[1], input.Item2[2], input.Item1);
                     var out1 = output.Zip(input.Item3, (a, b) => a == b);
 
-
                     if (out1.All(x => x))
                         same++;
+
+                    if (!out1.All(x => x))
+                    {
+                        possibleOpMapping[input.Item2[0]] = possibleOpMapping[input.Item2[0]].Where(x => x.GetType() != code.GetType()).ToList();
+                    }
                 }
                 if (same >= 3)
                     counter++;
             }
-            Console.WriteLine(counter);
+            Console.WriteLine("Part 1: " + counter);
 
+            OpCode[] finalMappings = GetOpCodeMapping(ref possibleOpMapping);
+            int[] registers = ExecuteCode(finalMappings);
+            Console.WriteLine("Part 2: " + registers[0]);
+
+        }
+
+        private static int[] ExecuteCode(OpCode[] finalMappings)
+        {
+            var registers = new int[] { 0, 0, 0, 0 };
+            foreach (var line in File.ReadAllLines("input2.txt"))
+            {
+                var values = line.Split(' ').Select(x => Int32.Parse(x)).ToArray();
+                registers[values[3]] = finalMappings[values[0]].Execute(values[1], values[2], registers);
+            }
+
+            return registers;
+        }
+
+        private static OpCode[] GetOpCodeMapping(ref List<List<OpCode>> possibleOpMapping)
+        {
+            var finalMappings = new OpCode[16];
+            while (finalMappings.Where(x => x == null).Any())
+            {
+                var knownOp = possibleOpMapping.Where(x => x.Count > 0).OrderBy(x => x.Count).First();
+                var opCode = possibleOpMapping.IndexOf(knownOp);
+                finalMappings[opCode] = knownOp.Single();
+                possibleOpMapping = possibleOpMapping.Select(y => y.Where(x => x.GetType() != knownOp.Single().GetType()).ToList()).ToList();
+            }
+
+            return finalMappings;
         }
 
         private static List<(int[], int[], int[])> GetInputs()
@@ -70,7 +108,7 @@ namespace Day16
             {
                 var input = lines[i].Split('[', ']', ',');
                 var arguments = lines[i + 1].Split(' ');
-                var ouput = lines[i+2].Split('[', ']', ',');
+                var ouput = lines[i + 2].Split('[', ']', ',');
                 result.Add((new int[] { Int32.Parse(input[1]), Int32.Parse(input[2]), Int32.Parse(input[3]), Int32.Parse(input[4]) },
                     new int[] { Int32.Parse(arguments[0]), Int32.Parse(arguments[1]), Int32.Parse(arguments[2]), Int32.Parse(arguments[3]) },
                     new int[] { Int32.Parse(ouput[1]), Int32.Parse(ouput[2]), Int32.Parse(ouput[3]), Int32.Parse(ouput[4]) }));
