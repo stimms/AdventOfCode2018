2b40424abd850c797b54d7257033029c3423c291
Simon Timms <stimms@gmail.com>
2018-12-16 21:12:54 -0700

    Start day 17

---
+++ Day17/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using Xunit;
+
+namespace Day17
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Part1();
+            Part2();
+
+            Console.WriteLine("done.");
+            Console.ReadLine();
+        }
+        
+        private static void Part1()
+        {
+          
+
+        }
+
+       private static void Part2()
+        {
+          
+
+        }
+}
