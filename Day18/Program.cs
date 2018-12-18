88c2def9724b2712d0727d18c58cfcc36958fb25
Simon Timms <stimms@gmail.com>
2018-12-17 21:36:47 -0700

    Outlines for the next few days

---
+++ Day18/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+
+namespace Day18
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Part1();
+            Part2();
+
+            Console.WriteLine("done.");
+            Console.ReadLine();
+        }
+
+        private static void Part1()
+        {
+            var lines = File.ReadAllLines("input1.txt");
+            
+        }
+
+
+
+        private static void Part2()
+        {
+
+
+        }
+    }
+
+}
